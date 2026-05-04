0ad5b7078dac71c422640fc2b0ad3d5f230a9fed
hamzaaaa-12 <hamzaaaa12007@gmail.com>
2026-05-04 04:49:56 +0300

    neww

---
+++ OS/src/GanttSlot.java
@@ -0,0 +1,11 @@
+public class GanttSlot {
+    public int processId;
+    public int startTime;
+    public int endTime;
+
+    public GanttSlot(int processId, int startTime, int endTime) {
+        this.processId = processId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
